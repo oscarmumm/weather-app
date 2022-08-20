9b63e38e09c51766d9ece27350c7c8f6e6d50d1f
Oscar Mumm <oscarmumm@gmail.com>
2022-08-20 16:42:48 -0300

    agregada varible principal del clima

--- js/app.js
+++ js/app.js
@@ -20,16 +20,33 @@ let fetchWeather = function(latitud, longitud) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apikey}&units=metric&lang=sp`)
         .then(response => response.json())
         .then((data) => {
-            console.log(data)
-            console.log("Ciudad / Pais: ", data.name +" / "+ data.sys.country)
-            console.log("Condicion climatica: ", data.weather[0].description)
-            console.log("Temperatura actual: ", data.main.temp)
-            console.log("Sensacion termica: ", data.main.feels_like)
-            console.log("Maxima: ", data.main.temp_max)
-            console.log("Minnima: ", data.main.temp_min)
-            console.log("Presion atmosferica: ", data.main.pressure)
-            console.log("Humedad: ", data.main.humidity)
-            console.log(data.weather)
+            return clima = {
+                ciudad: data.name,
+                pais: data.sys.country,
+                descripcion: data.weather[0].description,
+                temperaturaActual: data.main.temp,
+                sensacionTermica: data.main.feels_like,
+                temperaturaMaxima: data.main.temp_max,
+                temperaturaMinima: data.main.temp_min,
+                presion: data.main.pressure,
+                humedad: data.main.humidity,
+                visibilidad: (data.visibility)/1000,
+                velocidadViento: (data.wind.speed)*3.6,
+                direccionViento: data.wind.deg
+            }
+            // console.log(data)
+            // console.log("Ciudad / Pais: ", data.name +" / "+ data.sys.country)
+            // console.log("Condicion climatica: ", data.weather[0].description)
+            // console.log("Temperatura actual: ", data.main.temp)
+            // console.log("Sensacion termica: ", data.main.feels_like)
+            // console.log("Maxima: ", data.main.temp_max)
+            // console.log("Minnima: ", data.main.temp_min)
+            // console.log("Presion atmosferica: ", data.main.pressure)
+            // console.log("Humedad: ", data.main.humidity)
+            // console.log("Visibilidad: ", (data.visibility)/1000)//divido para pasarlo a km
+            // console.log("Velocidad del viento: ", (data.wind.speed)*3.6)//multiplico para pasarlo a km/h
+            // console.log("Direccion del viento: ", data.wind.deg)//viene expresado en grados (ver tabla)
+
         })
 }
 
@@ -39,6 +56,18 @@ botonBuscar.addEventListener("click", ()=>{
     fetchLocation(ubicacion)
 })
 
+
+/*
+tabla de viento:
+N:  337.5 a 360 y 0 a 22.5
+NE: 22.5 a 67.5
+E: 67.5 a 112.5
+SE: 112.5 a 157.5
+S: 157.5 a 202.5
+SO: 202.5 a 247.5
+O: 247.5 a 292.5
+NO: 292.5 a 337.5
+*/
 
 /*
 Api call para obtener las coordenadas de 
