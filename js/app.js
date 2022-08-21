617510f4b8720c6383b0fac4d1a414a22d586bb1
Oscar Mumm <oscarmumm@gmail.com>
2022-08-21 01:22:21 -0300

    agregando iconos y mostrando en interfaz

--- js/app.js
+++ js/app.js
@@ -3,14 +3,24 @@ const apikey = "1e0852abb42466a00a4d6d999c3e2b64"
 const buscador = document.getElementById("buscador")
 const botonBuscar = document.getElementById("boton-buscar")
 
+const ciudad = document.getElementById("ciudad")
+const pais = document.getElementById("pais")
+const icono = document.getElementById("icono-clima")
+const descripcion = document.getElementById("descripcion")
+const temperaturaActual = document.getElementById("temperatura-actual")
+const sensacionTermica = document.getElementById("sensacion-termica")
+const presion = document.getElementById("presion")
+const humedad = document.getElementById("humedad")
+const visibilidad = document.getElementById("visibilidad")
+const viento = document.getElementById("viento")
 
 let fetchLocation = function (ubicacion) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ubicacion}"&limit=1&appid=1e0852abb42466a00a4d6d999c3e2b64`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ubicacion}"&limit=1&appid=${apikey}`)
         .then((response) => {
             return response.json()
         })
         .then((data) => {
-            console.log('data:', data)
+            //console.log('data:', data)
             fetchWeather(data[0].lat, data[0].lon)
         })
     }
@@ -20,9 +30,13 @@ let fetchWeather = function(latitud, longitud) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apikey}&units=metric&lang=sp`)
         .then(response => response.json())
         .then((data) => {
+            console.log(data)
+            //buscarIcono(data.weather[0].id)
             return clima = {
+                //hora: calcularHora(data.timezone),
                 ciudad: data.name,
                 pais: data.sys.country,
+                icono: buscarIcono(calcularHora(data.timezone), data.weather[0].id),
                 descripcion: data.weather[0].description,
                 temperaturaActual: data.main.temp,
                 sensacionTermica: data.main.feels_like,
@@ -34,20 +48,34 @@ let fetchWeather = function(latitud, longitud) {
                 velocidadViento: (data.wind.speed)*3.6,
                 direccionViento: data.wind.deg
             }
-            // console.log(data)
-            // console.log("Ciudad / Pais: ", data.name +" / "+ data.sys.country)
-            // console.log("Condicion climatica: ", data.weather[0].description)
-            // console.log("Temperatura actual: ", data.main.temp)
-            // console.log("Sensacion termica: ", data.main.feels_like)
-            // console.log("Maxima: ", data.main.temp_max)
-            // console.log("Minnima: ", data.main.temp_min)
-            // console.log("Presion atmosferica: ", data.main.pressure)
-            // console.log("Humedad: ", data.main.humidity)
-            // console.log("Visibilidad: ", (data.visibility)/1000)//divido para pasarlo a km
-            // console.log("Velocidad del viento: ", (data.wind.speed)*3.6)//multiplico para pasarlo a km/h
-            // console.log("Direccion del viento: ", data.wind.deg)//viene expresado en grados (ver tabla)
-
         })
+        //poner un then aca
+        .then(clima => {
+            console.log(clima)
+            mostrarDatos(clima)
+        })
+}
+
+let mostrarDatos = function (clima) {
+    ciudad.innerText = `${clima.ciudad}`;
+
+    pais.innerText = `${clima.pais}`;
+
+    icono.src = `/icons/${clima.icono}`
+
+    descripcion.innerText = `${clima.descripcion}`;
+
+    temperaturaActual.innerText = `${clima.temperaturaActual} °C`;
+
+    sensacionTermica.innerText = `Sensación térmica ${clima.sensacionTermica} °C`;
+
+    presion.innerText = `Presión atmosférica: ${clima.presion} hpa`;
+
+    humedad.innerText = `Humedad: ${clima.humedad} %`;
+
+    visibilidad.innerText = `Visibilidad: ${clima.visibilidad} km`;
+
+    viento.innerText = `Viento: ${clima.velocidadViento} km/h`;
 }
 
 botonBuscar.addEventListener("click", ()=>{
@@ -56,37 +84,72 @@ botonBuscar.addEventListener("click", ()=>{
     fetchLocation(ubicacion)
 })
 
+let calcularHora = function (zona) {
+    const fecha = new Date()
+    let hora = fecha.getUTCHours()
+    hora = hora + (zona/3600)
+    if (hora > 24) {
+        hora = hora - 24
+        return hora
+    } else {
+        return hora
+    }
+}
 
-/*
-tabla de viento:
-N:  337.5 a 360 y 0 a 22.5
-NE: 22.5 a 67.5
-E: 67.5 a 112.5
-SE: 112.5 a 157.5
-S: 157.5 a 202.5
-SO: 202.5 a 247.5
-O: 247.5 a 292.5
-NO: 292.5 a 337.5
-*/
+let buscarIcono = function (hora, id) {
+    if (6 < hora && hora < 19) {
+        console.log(hora)
+        switch(id){
+            case (200 < id && id < 299):
+                return "11d.svg"
+                break;
+            case 300 < id && id < 399:
+                return "09d.svg"
+                break;
+            case 500 < id && id < 505:
+                return "10d.svg"
+                break;
+            case 511 < id && id < 699:
+                return "13d.svg"
+                break;
+            case 700 < id && id< 799:
+                return "50d.svg"
+                break;
+            case 800:
+                return "01d.svg"
+                break;
+            case 801:
+                return "02d.svg"
+                break;
+            case 802:
+                return "03d.svg"
+                break;
+            case 803:
+                return "04d.svg"
+                break;
+            case 804:
+                return "04d.svg"
+                break;
+        }
+    } else {
+        console.log(hora)
+        switch(id) {
+            case 800:
+                return "01n.svg"
+                break;
+            case 801:
+                return "02n.svg"
+                break;
+            case 802:
+                return "03n.svg"
+                break;
+            case 803:
+                return "04n.svg"
+                break;
+            case 804:
+                return "04n.svg"
+                break;
+        }
+    }
+}
 
-/*
-Api call para obtener las coordenadas de 
-latitud y longitud de una determinada ciudad:
-
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-Ejejmplo:
-http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-
-DOC: https://openweathermap.org/api/geocoding-api#direct
-
-
-Api call para obtener los datos meteorológicos:
-
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-Ejemplo:
-https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}
-
-DOC: https://openweathermap.org/current
-*/
